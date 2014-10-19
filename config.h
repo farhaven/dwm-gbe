4790ff6f0f8a9f9bab3c67f2dbcd2fc42562287f
Gregor Best <gbe@unobtanium.de>
2014-10-19 17:00:45 +0200

    Here be updates

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 // static const char font[] = "Droid Sans Mono for Powerline 9";
-static const char font[] = "Ubuntu Mono derivative Powerline 10.5";
+static const char font[] = "Terminess Powerline 9";
 static const char normbordercolor[] = "#ccc";
 static const char normbgcolor[]     = "#ccc";
 static const char normfgcolor[]     = "#000";
@@ -25,14 +25,15 @@ static const char *tags[] = {
 	"2:mail", "3:www", "4:term", "q:comm", "w:doc", "d", "e", "f"
 };
 static const Rule rules[] = {
-	/* class      instance       title    tags mask isfloating monitor focused opacity */
+	/* class      instance       title    tags mask isfloating monitor */
 	{ "Gimp",     NULL,          NULL,    1 << 5,   False,     -1 },
 	{ "Xmessage", NULL,          NULL,    0,        True,      -1 },
 	{ NULL,       "mutt",        NULL,    1 << 0,   False,     -1 },
 	{ "Surf",     NULL,          NULL,    1 << 1,   False,     -1 },
 	{ NULL,       "surf",        NULL,    1 << 1,   False,     -1 },
 	{ "Firefox",  NULL,          NULL,    1 << 1,   False,     -1 },
-	{ NULL,       "xterm-256color", NULL,    1 << 2,   False,  -1 },
+	{ NULL,       "xterm-256color", NULL, 1 << 2,   False,     -1 },
+	{ NULL,       "st-256color", NULL,    1 << 2,   False,     -1 },
 	{ NULL,       "irssi",       NULL,    1 << 3,   False,     -1 },
 	{ NULL,       "Pidgin",      NULL,    1 << 3,   False,     -1 },
 	{ "Evince",   NULL,          NULL,    1 << 4,   False,     -1 },
@@ -47,7 +48,7 @@ static const Rule rules[] = {
 	{ "NetHack",  "inventory",   NULL,    0,        True,      -1 },
 	{ "Thunderbird", "Mail",     NULL,    1 << 7,   False,     -1 },
 	{ "Thunderbird", "Calendar", NULL,    0,        True,      -1 },
-	{ "Korganizer", NULL,        NULL,    1 << 7,   False,     -1 },
+	{ "Tkremind", NULL,          NULL,    1 << 7,   False,     -1 },
 	{ "XConsole",  NULL,         NULL,    1 << 6,   False,     -1 },
 	{ "MPlayer",  NULL,          NULL,    1 << 6,   True,      -1 },
 	{ "Vlc",      NULL,          NULL,    1 << 6,   False,     -1 },
@@ -56,9 +57,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact      = 1.0 / 1.618; /* reverse of the golden cut */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const float mfact      = 0.58;
+static const int nmaster      = 1;
+static const Bool resizehints = True;
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -76,8 +77,11 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,  {.ui = 1 << TAG} },
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
+/* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0";
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font,
+	"-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor,
+	NULL };
 static const char *termcmd[] = { "fdb", NULL };
 // static const char *lockcmd[] = { "xscreensaver-command", "-lock", NULL};
 static const char *lockcmd[] = { "xscreensaver-command", "-lock", NULL};
@@ -91,20 +95,20 @@ static Key keys[] = {
 	{ MODKEY,             XK_Return,      spawn,      {.v = termcmd } },
 	{ MODKEY,             XK_l,           spawn,      {.v = lockcmd } },
 	{ MODKEY,             XK_b,           togglebar,  {0} },
-	{ MODKEY,             XK_Down, 	     focusstack, {.i = +1 } },
-	{ MODKEY,             XK_Up,   	     focusstack, {.i = -1 } },
-	{ MODKEY | Mod1Mask,  XK_Right,	     setmfact,   {.f = +0.05} },
-	{ MODKEY | Mod1Mask,  XK_Left, 	     setmfact,   {.f = -0.05} },
-	{ MODKEY,             XK_Left, 	     zoom,       {0} },
-	{ MODKEY,	          XK_Right,       pushdown,   {0} },
-	{ MODKEY,             XK_r,    	     view,       {0} },
+	{ MODKEY,             XK_Down,        focusstack, {.i = +1 } },
+	{ MODKEY,             XK_Up,          focusstack, {.i = -1 } },
+	{ MODKEY | Mod1Mask,  XK_Right,       setmfact,   {.f = +0.01} },
+	{ MODKEY | Mod1Mask,  XK_Left,        setmfact,   {.f = -0.01} },
+	{ MODKEY,             XK_Left,        zoom,       {0} },
+	{ MODKEY,             XK_Right,       pushdown,   {0} },
+	{ MODKEY,             XK_r,           view,       {0} },
 	{ MODKEY | ShiftMask, XK_c,           killclient, {0} },
 	{ MODKEY,             XK_0,           toggleview, {.ui = ~0 } },
-	{ MODKEY | ShiftMask, XK_0,	        toggletag,  {.ui = ~0 } },
+	{ MODKEY | ShiftMask, XK_0,           toggletag,  {.ui = ~0 } },
 	{ 0,                  XF86XK_Back,    focusmon,   {.i = -1 } },
 	{ 0,                  XF86XK_Forward, focusmon,   {.i = +1 } },
-	{ MODKEY, 	          XF86XK_Back,    tagmon,     {.i = -1 } },
-	{ MODKEY,   	       XF86XK_Forward, tagmon,     {.i = +1 } },
+	{ MODKEY,             XF86XK_Back,    tagmon,     {.i = -1 } },
+	{ MODKEY,             XF86XK_Forward, tagmon,     {.i = +1 } },
 	TAGKEYS(              XK_2,                       0)
 	TAGKEYS(              XK_3,                       1)
 	TAGKEYS(              XK_4,                       2)
@@ -112,21 +116,22 @@ static Key keys[] = {
 	TAGKEYS(              XK_w,                       4)
 	TAGKEYS(              XK_d,                       5)
 	TAGKEYS(              XK_e,                       6)
-	TAGKEYS(	             XK_f,			              7)
+	TAGKEYS(              XK_f,                       7)
 };
 
 /* button definitions */
-/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+/* click can be ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin,
+ * or ClkRootWin */
 static Button buttons[] = {
 	/* click         event mask button   function        argument */
 	{ ClkLtSymbol,   0,         Button1, setlayout,      {.v = &layouts[0]} },
-	{ ClkLtSymbol,	  0,		    Button2, setlayout,      {.v = &layouts[1]} },
+	{ ClkLtSymbol,   0,         Button2, setlayout,      {.v = &layouts[1]} },
 	{ ClkLtSymbol,   0,         Button3, setlayout,      {.v = &layouts[2]} },
 
 	{ ClkWinTitle,   0,         Button1, focusstack,     {.i = -1} },
-	{ ClkWinTitle,	  0,		    Button2, zoom,	        { 0 } },
-	{ ClkWinTitle,	  0,	       Button3, focusstack,     {.i = 1 } },
-	{ ClkStatusText, 0,		    Button2, killclient,	  {0} },
+	{ ClkWinTitle,   0,         Button2, zoom,           {0} },
+	{ ClkWinTitle,   0,         Button3, focusstack,     {.i = 1 } },
+	{ ClkStatusText, 0,         Button2, killclient,     {0} },
 	{ ClkStatusText, 0,         Button1, spawn,          {.v = termcmd } },
 
 	{ ClkClientWin,  MODKEY,    Button1, movemouse,      {0} },
