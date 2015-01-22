ae09f9096d5b987abffc692690bd265d2eb4e93b
Gregor Best <gbe@unobtanium.de>
2015-01-22 16:01:07 +0100

    Tiny fix for text shortening

--- drw.c
+++ drw.c
@@ -141,7 +141,7 @@ drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, const char *tex
 	ty = y + (h / 2) - (th / 2);
 	tx = x + (h / 2);
 	/* shorten text if necessary */
-	for(len = MIN(olen, sizeof buf); len && tex.w > w; len--)
+	for(len = MIN(olen, sizeof buf); len && tex.w >= w; len--)
 		drw_font_getexts(drw, text, len, &tex, markup);
 	if(!len)
 		return;
